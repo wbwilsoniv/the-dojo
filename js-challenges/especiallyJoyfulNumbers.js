6396164e40c088045a36a6abcc71a9208c726ff6
Will Wil <wbwilsoniv@gmail.com>
2022-10-06 11:54:57 -0400

    Solves initial tests for Harshad numbers. still needs to reverse and multiply digit sums

--- js-challenges/especiallyJoyfulNumbers.js
+++ js-challenges/especiallyJoyfulNumbers.js
@@ -9,7 +9,19 @@ and 19 * 91 = 1729 --> the number that we started with.
 Complete the function which tests if a positive integer n is Harshad number, and returns True if the product of its digit sum and its digit sum reversed equals n; otherwise return False.
 */
 
-
+// Solves initial tests for Harshad numbers. still needs to reverse and multiply digit sums.
 function numberJoy(n) {
-    // your code
+    let isHarshad = false;
+    let is
+    let digitArr = n.toString().split("");
+    console.log(digitArr);
+    let digitSum = 0;
+    for(let i = 0; i < digitArr.length; i++){
+      digitSum += (digitArr[i] * 1);
+      console.log(digitSum);
+    }
+    if(n % digitSum === 0){
+      isHarshad = true;
+    }
+    return isHarshad;
 }
