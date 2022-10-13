8f316265d3e9b2877d1397ea1ab2ae719dbd6c38
Will Wil <wbwilsoniv@gmail.com>
2022-10-13 11:59:11 -0700

    Refactored initial steps to find prices within budget. Sort method replaced with forloop over filtered array. Almost works

--- js-challenges/filterCoffee.js
+++ js-challenges/filterCoffee.js
@@ -10,6 +10,38 @@ Your 'search' function should return the stores that sell coffee within your bud
 The search function should return a string of prices for the coffees beans you can afford. The prices in this string are to be sorted in ascending order.
 */
 
+// Almost sorts array
+function search(budget, prices) {
+    const withinBudget = prices.filter(price => price <= budget);
+    let sortedPrices = [];
+    if(withinBudget.length > 0){
+      for(i = 0; i <= withinBudget.length-1; i++){
+        if(withinBudget[i] >= withinBudget[0]){
+          sortedPrices.push(withinBudget[i]);
+          console.log(sortedPrices);
+        } else {sortedPrices.unshift(withinBudget[i])}
+      }
+    }
+    /*for(i = 0; i < pricesSorted.length; i++){
+      if(pricesSorted[i] <= budget){
+        withinBudget.push(pricesSorted[i]);
+      }
+      console.log(withinBudget);
+    }
+    */
+   /* if(withinBudget.length > 0){
+    for(i = 0;i < withinBudget.length; i++){
+      if(withinBudget[i] >= withinBudget[0]){
+        pricesSorted.push(withinBudget[i])
+        console.log(pricesSorted);
+      } else {pricesSorted.unshift(withinBudget[i])}
+    }
+      }
+      */
+    console.log(sortedPrices.toString());
+    return sortedPrices.toString();
+  }
+
 // Kinda sorts the array, but not accurately.
 function search(budget, prices) {
     let withinBudget = [];
