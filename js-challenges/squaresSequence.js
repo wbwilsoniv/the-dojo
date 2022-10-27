499d6804f59501758b0185a20f6c63e6abced116
Will Wil <wbwilsoniv@gmail.com>
2022-10-27 12:03:13 -0400

    Added function that passes initial tests for squaresSequence

--- js-challenges/squaresSequence.js
+++ js-challenges/squaresSequence.js
@@ -7,6 +7,30 @@ Examples
 3, 3  -->  [3, 9, 81]
 */
 
+// Passes Initial Tests
 function squares(x, n) {
-    
+    let sqArr = [];
+    if(n <= 0){
+        return sqArr;
+    } else {
+        sqArr.push(x);
+        for(let i = 1; i < n; i++){
+            sqArr.push(sqArr[i - 1] ** 2);
+            console.log(sqArr[i - 1], sqArr);
+        }
+      return sqArr;
+    }
+}
+
+// Did not work
+function squares(x, n) {
+    let sqArr = [];
+    if(x <= 0){
+        return sqArr;
+    } else {
+        for(let i = 1; i <= n; i++){
+            sqArr.push(x ^ i);
+            console.log(sqArr);
+        }
+    }
 }
